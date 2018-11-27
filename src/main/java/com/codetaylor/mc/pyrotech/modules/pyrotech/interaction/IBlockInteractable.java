92021c8ac217c8b7298100164b79202f6bc979c6
Jason Taylor <jason@codetaylor.com>
2018-11-27 13:32:58 -0800

    Create interactable block interface

---
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/interaction/IBlockInteractable.java
@@ -0,0 +1,48 @@
+package com.codetaylor.mc.pyrotech.modules.pyrotech.interaction;
+
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+public interface IBlockInteractable {
+
+  default boolean interact(
+      World world,
+      BlockPos pos,
+      IBlockState state,
+      EntityPlayer player,
+      EnumHand hand,
+      EnumFacing facing,
+      float hitX,
+      float hitY,
+      float hitZ
+  ) {
+
+    TileEntity tileEntity = world.getTileEntity(pos);
+
+    if (tileEntity instanceof ITileInteractable) {
+      ITileInteractable tile = (ITileInteractable) tileEntity;
+      tile.interact(tile.asTileInteractable(), world, pos, state, player, hand, facing, hitX, hitY, hitZ);
+    }
+
+    return true;
+  }
+
+  default RayTraceResult interactionRayTrace(RayTraceResult result, IBlockState blockState, World world, BlockPos pos, Vec3d start, Vec3d end) {
+
+    TileEntity tileEntity = world.getTileEntity(pos);
+
+    if (tileEntity instanceof ITileInteractable) {
+      return InteractionRayTracer.collisionRayTrace(result, ((ITileInteractable) tileEntity).asTileInteractable(), blockState, world, pos, start, end);
+    }
+
+    return result;
+  }
+
+}
