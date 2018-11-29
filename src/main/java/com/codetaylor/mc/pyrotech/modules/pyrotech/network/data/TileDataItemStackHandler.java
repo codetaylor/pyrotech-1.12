520ffb82ab035499c4b00f151c597ea62c9171d7
Jason Taylor <jason@codetaylor.com>
2018-11-28 19:14:15 -0800

    Optimize networked stack handler to only serialize dirty slots

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/network/data/TileDataItemStackHandler.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/network/data/TileDataItemStackHandler.java
@@ -2,27 +2,35 @@ package com.codetaylor.mc.pyrotech.modules.pyrotech.network.data;
 
 import com.codetaylor.mc.pyrotech.modules.pyrotech.network.ITileDataItemStackHandler;
 import com.codetaylor.mc.pyrotech.modules.pyrotech.network.TileDataBase;
+import com.google.common.base.Preconditions;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraftforge.items.ItemStackHandler;
 
 import java.io.IOException;
+import java.util.BitSet;
 
 public class TileDataItemStackHandler<H extends ItemStackHandler & ITileDataItemStackHandler>
     extends TileDataBase {
 
   private H stackHandler;
+  private BitSet dirtySlots;
 
   public TileDataItemStackHandler(H stackHandler) {
 
     this(stackHandler, 1);
+    this.dirtySlots = new BitSet(stackHandler.getSlots());
   }
 
   public TileDataItemStackHandler(H stackHandler, int updateInterval) {
 
     super(updateInterval);
     this.stackHandler = stackHandler;
-    this.stackHandler.addObserver((handler, slot) -> this.setDirty(true));
+    this.stackHandler.addObserver((handler, slot) -> {
+      this.setDirty(true);
+      this.dirtySlots.set(slot);
+    });
     this.setDirty(true);
   }
 
@@ -32,19 +40,40 @@ public class TileDataItemStackHandler<H extends ItemStackHandler & ITileDataItem
   }
 
   @Override
+  public void setDirty(boolean dirty) {
+
+    super.setDirty(dirty);
+
+    if (!dirty) {
+      this.dirtySlots.clear();
+    }
+  }
+
+  @Override
   public void read(PacketBuffer buffer) throws IOException {
 
-    NBTTagCompound tag = buffer.readCompoundTag();
+    int dirtyCount = buffer.readInt();
 
-    if (tag != null) {
-      this.stackHandler.deserializeNBT(tag);
+    for (int i = 0; i < dirtyCount; i++) {
+      int slot = buffer.readInt();
+      this.stackHandler.setStackInSlot(slot, new ItemStack(Preconditions.checkNotNull(buffer.readCompoundTag())));
     }
   }
 
   @Override
   public void write(PacketBuffer buffer) {
 
-    buffer.writeCompoundTag(this.stackHandler.serializeNBT());
+    final int dirtyCount = this.dirtySlots.cardinality();
+
+    buffer.writeInt(dirtyCount);
+
+    if (dirtyCount > 0) {
+
+      for (int i = this.dirtySlots.nextSetBit(0); i >= 0; i = this.dirtySlots.nextSetBit(i + 1)) {
+        buffer.writeInt(i);
+        buffer.writeCompoundTag(this.stackHandler.getStackInSlot(i).serializeNBT());
+      }
+    }
   }
 
 }
