de499df8b19c17eae1bf4fe06e17eb45d5cf1413
Jason Taylor <jason@codetaylor.com>
2018-11-28 17:11:37 -0800

    Add tile data service network overlay

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/network/CPacketTileData.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/network/CPacketTileData.java
@@ -1,12 +1,12 @@
 package com.codetaylor.mc.pyrotech.modules.pyrotech.network;
 
 import com.codetaylor.mc.athenaeum.spi.packet.CPacketTileEntityBase;
-import com.codetaylor.mc.pyrotech.Reference;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 import net.minecraftforge.fml.relauncher.Side;
@@ -55,9 +55,7 @@ public class CPacketTileData
   @Override
   protected IMessage onMessage(CPacketTileData message, MessageContext ctx, TileEntity tileEntity) {
 
-    if (Reference.IS_DEV) {
-      System.out.println(String.format("[CLIENT] %s: %d bytes", tileEntity.getPos(), message.buffer.writerIndex()));
-    }
+    MinecraftForge.EVENT_BUS.post(new TileDataServicePacketReceivedEvent(message.blockPos, message.buffer.writerIndex()));
 
     if (tileEntity instanceof TileDataContainerBase) {
 
