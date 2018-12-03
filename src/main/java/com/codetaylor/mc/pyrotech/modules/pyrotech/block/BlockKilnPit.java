87c4d5799a223fc3ed9cd0d6a386dc476e2b5d6a
Jason Taylor <jason@codetaylor.com>
2018-12-03 09:28:10 -0800

    Fix pit kiln not lighting with flint and steel

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/block/BlockKilnPit.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/block/BlockKilnPit.java
@@ -17,6 +17,8 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
@@ -205,7 +207,10 @@ public class BlockKilnPit
 
     ItemStack heldItem = player.getHeldItemMainhand();
 
-    if (heldItem.getItem() instanceof ItemIgniterBase) {
+    Item item = heldItem.getItem();
+
+    if (item instanceof ItemIgniterBase
+        || item == Items.FLINT_AND_STEEL) {
       return false;
     }
 
