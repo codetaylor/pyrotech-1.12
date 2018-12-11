2b114e61bee125869d6a53f77680beeda5cdbdc6
Jason Taylor <jason@codetaylor.com>
2018-12-11 11:31:58 -0800

    Add stone rock generation

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/world/WorldGenerator.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/world/WorldGenerator.java
@@ -1,8 +1,13 @@
 package com.codetaylor.mc.pyrotech.modules.pyrotech.world;
 
+import com.codetaylor.mc.athenaeum.util.BlockHelper;
 import com.codetaylor.mc.pyrotech.modules.pyrotech.ModulePyrotechConfig;
 import com.codetaylor.mc.pyrotech.modules.pyrotech.block.BlockOre;
+import com.codetaylor.mc.pyrotech.modules.pyrotech.block.BlockRock;
 import com.codetaylor.mc.pyrotech.modules.pyrotech.init.ModuleBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -42,7 +47,8 @@ public class WorldGenerator
   public void generate(Random random, int chunkX, int chunkZ, World world, IChunkGenerator chunkGenerator, IChunkProvider chunkProvider) {
 
     // Fossil
-    if (ModulePyrotechConfig.WORLD_GEN.FOSSIL.ENABLED) {
+    if (ModulePyrotechConfig.WORLD_GEN.FOSSIL.ENABLED
+        && ModulePyrotechConfig.WORLD_GEN.FOSSIL.CHANCES_TO_SPAWN > 0) {
       final int chancesToSpawn = ModulePyrotechConfig.WORLD_GEN.FOSSIL.CHANCES_TO_SPAWN;
       final int minY = ModulePyrotechConfig.WORLD_GEN.FOSSIL.MIN_HEIGHT;
       final int maxY = ModulePyrotechConfig.WORLD_GEN.FOSSIL.MAX_HEIGHT;
@@ -58,7 +64,8 @@ public class WorldGenerator
     }
 
     // Limestone
-    if (ModulePyrotechConfig.WORLD_GEN.LIMESTONE.ENABLED) {
+    if (ModulePyrotechConfig.WORLD_GEN.LIMESTONE.ENABLED
+        && ModulePyrotechConfig.WORLD_GEN.LIMESTONE.CHANCES_TO_SPAWN > 0) {
       final int chancesToSpawn = ModulePyrotechConfig.WORLD_GEN.LIMESTONE.CHANCES_TO_SPAWN;
       final int minY = ModulePyrotechConfig.WORLD_GEN.LIMESTONE.MIN_HEIGHT;
       final int maxY = ModulePyrotechConfig.WORLD_GEN.LIMESTONE.MAX_HEIGHT;
@@ -72,6 +79,37 @@ public class WorldGenerator
         this.worldGenLimestone.generate(world, random, new BlockPos(posX, posY, posZ));
       }
     }
+
+    // Stone Rocks
+    {
+      final int chancesToSpawn = 4;
+      final int blockXPos = chunkX << 4;
+      final int blockZPos = chunkZ << 4;
+      final double density = 0.0625;
+
+      for (int i = 0; i < chancesToSpawn; i++) {
+
+        int posX = blockXPos + random.nextInt(16) + 8;
+        int posY = world.getHeight(blockXPos, blockZPos);
+        int posZ = blockZPos + random.nextInt(16) + 8;
+
+        BlockHelper.forBlocksInCube(world, new BlockPos(posX, posY, posZ), 4, 4, 4, (w, p, bs) -> {
+
+          if (w.isAirBlock(p) && this.canSpawnOnTopOf(w, p.down(), w.getBlockState(p.down())) && random.nextFloat() < density) {
+            world.setBlockState(p, ModuleBlocks.ROCK.getDefaultState().withProperty(BlockRock.VARIANT, BlockRock.EnumType.STONE), 2 | 16);
+          }
+
+          return true; // keep processing
+        });
+      }
+    }
+
+  }
+
+  private boolean canSpawnOnTopOf(World world, BlockPos pos, IBlockState blockState) {
+
+    Block block = blockState.getBlock();
+    return block == Blocks.DIRT || block == Blocks.GRASS || block == Blocks.STONE;
   }
 
 }
