0a82c3978bc424174be43acc0c0e4b10dc1264be
Jason Taylor <jason@codetaylor.com>
2018-12-24 10:39:13 -0800

    Fix straw block should reduce fall damage

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/block/BlockThatch.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/block/BlockThatch.java
@@ -3,7 +3,10 @@ package com.codetaylor.mc.pyrotech.modules.pyrotech.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockThatch
     extends Block {
@@ -17,6 +20,12 @@ public class BlockThatch
     this.setHardness(0.5F);
     this.setHarvestLevel(null, 0);
     Blocks.FIRE.setFireInfo(this, 60, 100);
+  }
+
+  @Override
+  public void onFallenUpon(World world, BlockPos pos, Entity entity, float fallDistance) {
+
+    entity.fall(fallDistance, 0.2F);
   }
 
 }
