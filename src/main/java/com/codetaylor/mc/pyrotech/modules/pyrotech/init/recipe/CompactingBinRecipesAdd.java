81e3320f3cc3654ab624da07f226c5c9813b3f26
Jason Taylor <jason@codetaylor.com>
2019-01-09 14:20:50 -0800

    Fix compacting bin recipes

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/init/recipe/CompactingBinRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/init/recipe/CompactingBinRecipesAdd.java
@@ -16,60 +16,67 @@ public class CompactingBinRecipesAdd {
 
     // Gravel
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.GRAVEL),
         Ingredient.fromStacks(
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.STONE.getMeta()),
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.DIORITE.getMeta()),
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.GRANITE.getMeta()),
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.ANDESITE.getMeta())
-        ), new ItemStack(Blocks.GRAVEL),
+        ),
         8
     ).setRegistryName(ModulePyrotech.MOD_ID, "gravel"));
 
     // Dirt
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.DIRT, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.DIRT.getMeta())
-        ), new ItemStack(Blocks.DIRT, 1, 0),
+        ),
         8
     ).setRegistryName(ModulePyrotech.MOD_ID, "dirt"));
 
     // Sand
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.SAND, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(ModuleBlocks.ROCK, 1, BlockRock.EnumType.SAND.getMeta())
-        ), new ItemStack(Blocks.SAND, 1, 0),
+        ),
         8
     ).setRegistryName(ModulePyrotech.MOD_ID, "sand"));
 
     // Grass
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.GRASS, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(ModuleBlocks.ROCK_GRASS, 1, 0)
-        ), new ItemStack(Blocks.GRASS, 1, 0),
+        ),
         8
     ).setRegistryName(ModulePyrotech.MOD_ID, "grass"));
 
     // Clay
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.CLAY, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(Items.CLAY_BALL, 1, 0)
-        ), new ItemStack(Blocks.CLAY, 1, 0),
+        ),
         4
     ).setRegistryName(ModulePyrotech.MOD_ID, "clay"));
 
     // Snow
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.SNOW, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(Items.SNOWBALL, 1, 0)
-        ), new ItemStack(Blocks.SNOW, 1, 0),
+        ),
         4
     ).setRegistryName(ModulePyrotech.MOD_ID, "snow"));
 
     // Bone Block
     registry.register(new CompactingBinRecipe(
+        new ItemStack(Blocks.BONE_BLOCK, 1, 0),
         Ingredient.fromStacks(
             new ItemStack(Items.DYE, 1, 15)
-        ), new ItemStack(Blocks.BONE_BLOCK, 1, 0),
+        ),
         8
     ).setRegistryName(ModulePyrotech.MOD_ID, "bone_block"));
   }
