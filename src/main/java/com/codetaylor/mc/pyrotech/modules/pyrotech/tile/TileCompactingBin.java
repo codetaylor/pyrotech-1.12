cc4bc2b1977a94719e2fc1cb173a0b190347100d
Jason Taylor <jason@codetaylor.com>
2019-01-19 13:18:20 -0800

    Move dynamic stack handler to lib

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/tile/TileCompactingBin.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/tile/TileCompactingBin.java
@@ -1,10 +1,9 @@
 package com.codetaylor.mc.pyrotech.modules.pyrotech.tile;
 
-import com.codetaylor.mc.athenaeum.inventory.ObservableStackHandler;
+import com.codetaylor.mc.athenaeum.inventory.DynamicStackHandler;
 import com.codetaylor.mc.athenaeum.network.tile.data.TileDataFloat;
 import com.codetaylor.mc.athenaeum.network.tile.data.TileDataItemStackHandler;
 import com.codetaylor.mc.athenaeum.network.tile.spi.ITileData;
-import com.codetaylor.mc.athenaeum.network.tile.spi.ITileDataItemStackHandler;
 import com.codetaylor.mc.athenaeum.util.ArrayHelper;
 import com.codetaylor.mc.athenaeum.util.StackHelper;
 import com.codetaylor.mc.pyrotech.library.util.Util;
@@ -34,7 +33,6 @@ import net.minecraft.world.World;
 import net.minecraftforge.items.ItemStackHandler;
 
 import javax.annotation.Nonnull;
-import java.util.ArrayList;
 
 public class TileCompactingBin
     extends TileNetBase
@@ -437,90 +435,4 @@ public class TileCompactingBin
       return ItemStack.EMPTY;
     }
   }
-
-  private class DynamicStackHandler
-      extends ObservableStackHandler
-      implements ITileDataItemStackHandler {
-
-    public DynamicStackHandler(int initialSize) {
-
-      super(1);
-      this.stacks = new ItemStackList(initialSize);
-    }
-
-    @Override
-    public void setSize(int size) {
-
-      this.stacks = new ItemStackList(size);
-    }
-
-    @Nonnull
-    @Override
-    public ItemStack insertItem(int slot, @Nonnull ItemStack stack, boolean simulate) {
-
-      while (this.getSlots() - 1 < slot) {
-        this.stacks.add(ItemStack.EMPTY);
-      }
-
-      return super.insertItem(slot, stack, simulate);
-    }
-
-    @Nonnull
-    @Override
-    public ItemStack extractItem(int slot, int amount, boolean simulate) {
-
-      if (slot > this.getSlots() - 1) {
-        return ItemStack.EMPTY;
-      }
-
-      return super.extractItem(slot, amount, simulate);
-    }
-
-    @Override
-    public void setStackInSlot(int slot, @Nonnull ItemStack stack) {
-
-      while (this.getSlots() - 1 < slot) {
-        this.stacks.add(ItemStack.EMPTY);
-      }
-
-      super.setStackInSlot(slot, stack);
-    }
-
-    public void insertItem(ItemStack itemStack, boolean simulate) {
-
-      ItemStack remaining = itemStack;
-      int i = 0;
-
-      while (!remaining.isEmpty()) {
-
-        while (this.getSlots() - 1 < i) {
-          this.stacks.add(ItemStack.EMPTY);
-        }
-
-        remaining = super.insertItem(i, remaining, simulate);
-        i += 1;
-      }
-    }
-
-    public void clearStacks() {
-
-      for (int i = 0; i < this.getSlots(); i++) {
-        this.extractItem(i, this.getSlotLimit(i), false);
-      }
-    }
-
-    private class ItemStackList
-        extends NonNullList<ItemStack> {
-
-      public ItemStackList(int size) {
-
-        super(new ArrayList<>(size), ItemStack.EMPTY);
-
-        for (int i = 0; i < size; i++) {
-          this.add(ItemStack.EMPTY);
-        }
-      }
-    }
-  }
-
 }
