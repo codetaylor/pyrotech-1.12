bd8b3cddac8f37b0c29fcaec6ba66c694fa952ff
Jason Taylor <jason@codetaylor.com>
2019-01-21 10:12:10 -0800

    Allow tongs to pick up a dropped bloom from offhand

--- src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/event/ItemPickupEventHandler.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/pyrotech/event/ItemPickupEventHandler.java
@@ -4,6 +4,7 @@ import com.codetaylor.mc.pyrotech.modules.pyrotech.init.ModuleBlocks;
 import com.codetaylor.mc.pyrotech.modules.pyrotech.init.ModuleItems;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
@@ -19,17 +20,36 @@ public class ItemPickupEventHandler {
 
     EntityItem entityItem = event.getItem();
     ItemStack pickedUp = entityItem.getItem();
+
+    if (pickedUp.getItem() != Item.getItemFromBlock(ModuleBlocks.BLOOM)) {
+      return;
+    }
+
     EntityPlayer entityPlayer = event.getEntityPlayer();
     ItemStack heldItem = entityPlayer.getHeldItemMainhand();
+    boolean isOffhand = false;
 
-    if (heldItem.getItem() == ModuleItems.TONGS
-        && pickedUp.getItem() == Item.getItemFromBlock(ModuleBlocks.BLOOM)) {
-      ItemStack itemStack = new ItemStack(ModuleItems.TONGS_FULL, 1, heldItem.getMetadata());
-      itemStack.setTagCompound(pickedUp.getTagCompound());
-      heldItem.shrink(1);
-      ItemHandlerHelper.giveItemToPlayer(entityPlayer, itemStack);
-      event.getItem().setDead();
-      event.setCanceled(true);
+    if (heldItem.getItem() != ModuleItems.TONGS) {
+      heldItem = entityPlayer.getHeldItemOffhand();
+      isOffhand = true;
+    }
+
+    if (heldItem.getItem() != ModuleItems.TONGS) {
+      return;
+    }
+
+    event.getItem().setDead();
+    event.setCanceled(true);
+
+    ItemStack itemStack = new ItemStack(ModuleItems.TONGS_FULL, 1, heldItem.getMetadata());
+    itemStack.setTagCompound(pickedUp.getTagCompound());
+    heldItem.shrink(1);
+
+    if (isOffhand) {
+      entityPlayer.setItemStackToSlot(EntityEquipmentSlot.OFFHAND, itemStack);
+
+    } else {
+      ItemHandlerHelper.giveItemToPlayer(entityPlayer, itemStack, entityPlayer.inventory.currentItem);
     }
   }
 
