1399ffa930502bec3f844e0a7433d6f6582c0b00
Jason Taylor <jason@codetaylor.com>
2019-02-07 10:47:57 -0800

    Add more lava recipes for brick crucible

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/BrickCrucibleRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/BrickCrucibleRecipesAdd.java
@@ -2,10 +2,15 @@ package com.codetaylor.mc.pyrotech.modules.tech.machine.init.recipe;
 
 import com.codetaylor.mc.athenaeum.recipe.CompoundIngredientPublic;
 import com.codetaylor.mc.athenaeum.util.RecipeHelper;
+import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
+import com.codetaylor.mc.pyrotech.modules.core.block.BlockCobblestone;
+import com.codetaylor.mc.pyrotech.modules.core.block.BlockRock;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.ModuleTechMachine;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.ModuleTechMachineConfig;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.recipe.BrickCrucibleRecipe;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.recipe.StoneCrucibleRecipe;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
@@ -20,15 +25,52 @@ public class BrickCrucibleRecipesAdd {
   public static void apply(IForgeRegistry<BrickCrucibleRecipe> registry) {
 
     // Lava
-    //noinspection unchecked
+    int lavaTimeOneBlockToOneQuarterBucket = 2 * 60 * 20;
+
     registry.register(new BrickCrucibleRecipe(
         new FluidStack(FluidRegistry.LAVA, 250),
-        new CompoundIngredientPublic(Arrays.asList(new Ingredient[]{
+        new CompoundIngredientPublic(Arrays.asList(
             new OreIngredient("stone"),
-            new OreIngredient("cobblestone")
-        })),
-        2 * 60 * 20
+            new OreIngredient("stoneAndesite"),
+            new OreIngredient("stoneAndesitePolished"),
+            new OreIngredient("stoneDiorite"),
+            new OreIngredient("stoneDioritePolished"),
+            new OreIngredient("stoneGranite"),
+            new OreIngredient("stoneGranitePolished"),
+            new OreIngredient("cobblestone"),
+            Ingredient.fromStacks(
+                new ItemStack(ModuleCore.Blocks.COBBLESTONE, 1, BlockCobblestone.EnumType.ANDESITE.getMeta()),
+                new ItemStack(ModuleCore.Blocks.COBBLESTONE, 1, BlockCobblestone.EnumType.DIORITE.getMeta()),
+                new ItemStack(ModuleCore.Blocks.COBBLESTONE, 1, BlockCobblestone.EnumType.GRANITE.getMeta()),
+                new ItemStack(ModuleCore.Blocks.COBBLESTONE, 1, BlockCobblestone.EnumType.LIMESTONE.getMeta())
+            )
+        )),
+        lavaTimeOneBlockToOneQuarterBucket
     ).setRegistryName(ModuleTechMachine.MOD_ID, "lava_from_stone"));
+
+    registry.register(new BrickCrucibleRecipe(
+        new FluidStack(FluidRegistry.LAVA, 250),
+        Ingredient.fromStacks(new ItemStack(Blocks.GRAVEL)),
+        lavaTimeOneBlockToOneQuarterBucket
+    ).setRegistryName(ModuleTechMachine.MOD_ID, "lava_from_gravel"));
+
+    registry.register(new BrickCrucibleRecipe(
+        new FluidStack(FluidRegistry.LAVA, 250),
+        Ingredient.fromStacks(new ItemStack(Blocks.NETHERRACK)),
+        lavaTimeOneBlockToOneQuarterBucket / 2
+    ).setRegistryName(ModuleTechMachine.MOD_ID, "lava_from_netherrack"));
+
+    registry.register(new BrickCrucibleRecipe(
+        new FluidStack(FluidRegistry.LAVA, 25),
+        Ingredient.fromStacks(
+            new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.STONE.getMeta()),
+            new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.GRANITE.getMeta()),
+            new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.DIORITE.getMeta()),
+            new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.ANDESITE.getMeta()),
+            new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.LIMESTONE.getMeta())
+        ),
+        lavaTimeOneBlockToOneQuarterBucket / 8
+    ).setRegistryName(ModuleTechMachine.MOD_ID, "lava_from_rocks"));
   }
 
   public static void registerInheritedRecipes(
