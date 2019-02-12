a293c36ac9643c0ad1d9eb94417d594af5b111bb
Jason Taylor <jason@codetaylor.com>
2019-02-11 16:44:34 -0800

    Fix bucket item initialization

--- src/main/java/com/codetaylor/mc/pyrotech/modules/bucket/init/ItemInitializer.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/bucket/init/ItemInitializer.java
@@ -3,7 +3,6 @@ package com.codetaylor.mc.pyrotech.modules.bucket.init;
 import com.codetaylor.mc.athenaeum.registry.Registry;
 import com.codetaylor.mc.athenaeum.util.ModelRegistrationHelper;
 import com.codetaylor.mc.pyrotech.modules.bucket.ModuleBucket;
-import com.codetaylor.mc.pyrotech.modules.bucket.ModuleBucketConfig;
 import com.codetaylor.mc.pyrotech.modules.bucket.item.*;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -13,18 +12,10 @@ public final class ItemInitializer {
 
   public static void onRegister(Registry registry) {
 
-    if (ModuleBucketConfig.BUCKET_WOOD.ENABLED) {
-      registry.registerItem(new ItemBucketWood(), ItemBucketWood.NAME);
-    }
-
-    if (ModuleBucketConfig.BUCKET_CLAY.ENABLED) {
-      registry.registerItem(new ItemBucketClay(), ItemBucketClay.NAME);
-      registry.registerItem(new ItemBucketClayUnfired(), ItemBucketClayUnfired.NAME);
-    }
-
-    if (ModuleBucketConfig.BUCKET_STONE.ENABLED) {
-      registry.registerItem(new ItemBucketStone(), ItemBucketStone.NAME);
-    }
+    registry.registerItem(new ItemBucketWood(), ItemBucketWood.NAME);
+    registry.registerItem(new ItemBucketClay(), ItemBucketClay.NAME);
+    registry.registerItem(new ItemBucketClayUnfired(), ItemBucketClayUnfired.NAME);
+    registry.registerItem(new ItemBucketStone(), ItemBucketStone.NAME);
   }
 
   @SideOnly(Side.CLIENT)
@@ -32,39 +23,29 @@ public final class ItemInitializer {
 
     registry.registerClientModelRegistrationStrategy(() -> {
 
-      if (ModuleBucketConfig.BUCKET_WOOD.ENABLED) {
-        ModelRegistrationHelper.registerItemModels(
-            ModuleBucket.Items.BUCKET_WOOD
-        );
-        ModelRegistrationHelper.registerItemModel(
-            ModuleBucket.Items.BUCKET_WOOD,
-            ItemBucketBase.EnumType.MILK.getMeta(),
-            ItemBucketBase.EnumType.MILK.getName()
-        );
-      }
+      ModelRegistrationHelper.registerItemModels(
+          ModuleBucket.Items.BUCKET_WOOD,
+          ModuleBucket.Items.BUCKET_CLAY,
+          ModuleBucket.Items.BUCKET_CLAY_UNFIRED,
+          ModuleBucket.Items.BUCKET_STONE
+      );
+      ModelRegistrationHelper.registerItemModel(
+          ModuleBucket.Items.BUCKET_WOOD,
+          ItemBucketBase.EnumType.MILK.getMeta(),
+          ItemBucketBase.EnumType.MILK.getName()
+      );
 
-      if (ModuleBucketConfig.BUCKET_CLAY.ENABLED) {
-        ModelRegistrationHelper.registerItemModels(
-            ModuleBucket.Items.BUCKET_CLAY,
-            ModuleBucket.Items.BUCKET_CLAY_UNFIRED
-        );
-        ModelRegistrationHelper.registerItemModel(
-            ModuleBucket.Items.BUCKET_CLAY,
-            ItemBucketBase.EnumType.MILK.getMeta(),
-            ItemBucketBase.EnumType.MILK.getName()
-        );
-      }
+      ModelRegistrationHelper.registerItemModel(
+          ModuleBucket.Items.BUCKET_CLAY,
+          ItemBucketBase.EnumType.MILK.getMeta(),
+          ItemBucketBase.EnumType.MILK.getName()
+      );
 
-      if (ModuleBucketConfig.BUCKET_STONE.ENABLED) {
-        ModelRegistrationHelper.registerItemModels(
-            ModuleBucket.Items.BUCKET_STONE
-        );
-        ModelRegistrationHelper.registerItemModel(
-            ModuleBucket.Items.BUCKET_STONE,
-            ItemBucketBase.EnumType.MILK.getMeta(),
-            ItemBucketBase.EnumType.MILK.getName()
-        );
-      }
+      ModelRegistrationHelper.registerItemModel(
+          ModuleBucket.Items.BUCKET_STONE,
+          ItemBucketBase.EnumType.MILK.getMeta(),
+          ItemBucketBase.EnumType.MILK.getName()
+      );
     });
   }
 
