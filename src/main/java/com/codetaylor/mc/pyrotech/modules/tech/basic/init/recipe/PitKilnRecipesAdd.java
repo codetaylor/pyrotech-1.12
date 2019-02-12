775477d2b1d345c417b6e96b022f173a08233272
Jason Taylor <jason@codetaylor.com>
2019-02-11 16:42:21 -0800

    Fix clay bucket recipe registration

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/PitKilnRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/PitKilnRecipesAdd.java
@@ -3,7 +3,6 @@ package com.codetaylor.mc.pyrotech.modules.tech.basic.init.recipe;
 import com.codetaylor.mc.pyrotech.ModPyrotech;
 import com.codetaylor.mc.pyrotech.Reference;
 import com.codetaylor.mc.pyrotech.modules.bucket.ModuleBucket;
-import com.codetaylor.mc.pyrotech.modules.bucket.ModuleBucketConfig;
 import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
 import com.codetaylor.mc.pyrotech.modules.core.block.BlockCobblestone;
 import com.codetaylor.mc.pyrotech.modules.core.block.BlockRock;
@@ -22,8 +21,7 @@ public class PitKilnRecipesAdd {
   public static void apply(IForgeRegistry<KilnPitRecipe> registry) {
 
     // Clay Bucket
-    if (ModPyrotech.INSTANCE.isModuleEnabled(ModuleBucket.class)
-        && ModuleBucketConfig.BUCKET_CLAY.ENABLED) {
+    if (ModPyrotech.INSTANCE.isModuleEnabled(ModuleBucket.class)) {
       registry.register(new KilnPitRecipe(
           new ItemStack(ModuleBucket.Items.BUCKET_CLAY),
           Ingredient.fromStacks(new ItemStack(ModuleBucket.Items.BUCKET_CLAY_UNFIRED)),
