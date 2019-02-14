43c627b5ec1c59a723308f824307e8ca161dba65
Jason Taylor <jason@codetaylor.com>
2019-02-14 13:21:34 -0800

    Move stone cutting recipes to brick sawmill, rebalance recipe duration

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/BrickSawmillRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/BrickSawmillRecipesAdd.java
@@ -1,20 +1,117 @@
 package com.codetaylor.mc.pyrotech.modules.tech.machine.init.recipe;
 
 import com.codetaylor.mc.athenaeum.util.RecipeHelper;
+import com.codetaylor.mc.pyrotech.modules.core.item.ItemMaterial;
+import com.codetaylor.mc.pyrotech.modules.tech.machine.ModuleTechMachine;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.ModuleTechMachineConfig;
 import com.codetaylor.mc.pyrotech.modules.tech.machine.recipe.BrickSawmillRecipe;
-import com.codetaylor.mc.pyrotech.modules.tech.machine.recipe.StoneSawmillRecipe;
-import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public class BrickSawmillRecipesAdd {
 
-  public static void apply(IForgeRegistry<BrickSawmillRecipe> registry) {
-    //
+  public static void apply(IForgeRegistryModifiable<BrickSawmillRecipe> registry) {
+
+    // --- Stone ---
+
+    // Sandstone Slab
+    registerSawmillRecipeStone(registry, "sandstone_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 1),
+        Ingredient.fromStacks(
+            new ItemStack(Blocks.SANDSTONE, 1, 0),
+            new ItemStack(Blocks.SANDSTONE, 1, 1),
+            new ItemStack(Blocks.SANDSTONE, 1, 2)
+        )
+    );
+
+    // Red Sandstone Slab
+    registerSawmillRecipeStone(registry, "red_sandstone_slab",
+        new ItemStack(Blocks.STONE_SLAB2, 2, 0),
+        Ingredient.fromStacks(
+            new ItemStack(Blocks.RED_SANDSTONE, 1, 0),
+            new ItemStack(Blocks.RED_SANDSTONE, 1, 1),
+            new ItemStack(Blocks.RED_SANDSTONE, 1, 2)
+        )
+    );
+
+    // Brick Slab
+    registerSawmillRecipeStone(registry, "brick_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 4),
+        Ingredient.fromStacks(
+            new ItemStack(Blocks.BRICK_BLOCK)
+        )
+    );
+
+    // Nether Brick Slab
+    registerSawmillRecipeStone(registry, "nether_brick_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 6),
+        Ingredient.fromStacks(
+            new ItemStack(Blocks.NETHER_BRICK)
+        )
+    );
+
+    // Quartz Slab
+    registerSawmillRecipeStone(registry, "quartz_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 7),
+        Ingredient.fromStacks(
+            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 0),
+            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 1),
+            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 2)
+        )
+    );
+
+    // Purpur Slab
+    registerSawmillRecipeStone(registry, "purpur_slab",
+        new ItemStack(Blocks.PURPUR_SLAB, 2, 0),
+        Ingredient.fromStacks(new ItemStack(Blocks.PURPUR_BLOCK, 1, 0))
+    );
+
+    registerSawmillRecipeStone(registry, "stone_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 0),
+        Ingredient.fromStacks(new ItemStack(Blocks.STONE, 1, 0))
+    );
+
+    registerSawmillRecipeStone(registry, "cobblestone_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 3),
+        Ingredient.fromStacks(new ItemStack(Blocks.COBBLESTONE, 1, 0))
+    );
+
+    registerSawmillRecipeStone(registry, "stone_bricks_slab",
+        new ItemStack(Blocks.STONE_SLAB, 2, 5),
+        Ingredient.fromStacks(new ItemStack(Blocks.STONEBRICK, 1, 0))
+    );
+
+    registerSawmillRecipeStone(registry, "stone_brick",
+        ItemMaterial.EnumType.BRICK_STONE.asStack(2),
+        Ingredient.fromStacks(new ItemStack(Blocks.STONE_SLAB, 1, 5))
+    );
+
+  }
+
+  private static void registerSawmillRecipeStone(IForgeRegistryModifiable<BrickSawmillRecipe> registry, String name, ItemStack output, Ingredient input) {
+
+    registry.register(new BrickSawmillRecipe(
+        output.copy(),
+        input,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 8 * 20,
+        Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.IRON_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
+        0
+    ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_iron"));
+
+    registry.register(new BrickSawmillRecipe(
+        output.copy(),
+        input,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 4 * 20,
+        Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.DIAMOND_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
+        0
+    ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_diamond"));
   }
 
   public static void registerInheritedRecipes(
-      IForgeRegistryModifiable<StoneSawmillRecipe> stoneRegistry,
+      IForgeRegistryModifiable<BrickSawmillRecipe> stoneRegistry,
       IForgeRegistryModifiable<BrickSawmillRecipe> brickRegistry
   ) {
 
--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/StoneSawmillRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/machine/init/recipe/StoneSawmillRecipesAdd.java
@@ -81,80 +81,6 @@ public class StoneSawmillRecipesAdd {
         Ingredient.fromStacks(new ItemStack(Blocks.PLANKS, 1, 5))
     );
 
-    // --- Stone ---
-
-    // Sandstone Slab
-    registerSawmillRecipeStone(registry, "sandstone_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 1),
-        Ingredient.fromStacks(
-            new ItemStack(Blocks.SANDSTONE, 1, 0),
-            new ItemStack(Blocks.SANDSTONE, 1, 1),
-            new ItemStack(Blocks.SANDSTONE, 1, 2)
-        )
-    );
-
-    // Red Sandstone Slab
-    registerSawmillRecipeStone(registry, "red_sandstone_slab",
-        new ItemStack(Blocks.STONE_SLAB2, 2, 0),
-        Ingredient.fromStacks(
-            new ItemStack(Blocks.RED_SANDSTONE, 1, 0),
-            new ItemStack(Blocks.RED_SANDSTONE, 1, 1),
-            new ItemStack(Blocks.RED_SANDSTONE, 1, 2)
-        )
-    );
-
-    // Brick Slab
-    registerSawmillRecipeStone(registry, "brick_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 4),
-        Ingredient.fromStacks(
-            new ItemStack(Blocks.BRICK_BLOCK)
-        )
-    );
-
-    // Nether Brick Slab
-    registerSawmillRecipeStone(registry, "nether_brick_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 6),
-        Ingredient.fromStacks(
-            new ItemStack(Blocks.NETHER_BRICK)
-        )
-    );
-
-    // Quartz Slab
-    registerSawmillRecipeStone(registry, "quartz_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 7),
-        Ingredient.fromStacks(
-            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 0),
-            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 1),
-            new ItemStack(Blocks.QUARTZ_BLOCK, 1, 2)
-        )
-    );
-
-    // Purpur Slab
-    registerSawmillRecipeStone(registry, "purpur_slab",
-        new ItemStack(Blocks.PURPUR_SLAB, 2, 0),
-        Ingredient.fromStacks(new ItemStack(Blocks.PURPUR_BLOCK, 1, 0))
-    );
-
-    registerSawmillRecipeStone(registry, "stone_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 0),
-        Ingredient.fromStacks(new ItemStack(Blocks.STONE, 1, 0))
-    );
-
-    registerSawmillRecipeStone(registry, "cobblestone_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 3),
-        Ingredient.fromStacks(new ItemStack(Blocks.COBBLESTONE, 1, 0))
-    );
-
-    registerSawmillRecipeStone(registry, "stone_bricks_slab",
-        new ItemStack(Blocks.STONE_SLAB, 2, 5),
-        Ingredient.fromStacks(new ItemStack(Blocks.STONEBRICK, 1, 0))
-    );
-
-    registerSawmillRecipeStone(registry, "stone_brick",
-        ItemMaterial.EnumType.BRICK_STONE.asStack(2),
-        Ingredient.fromStacks(new ItemStack(Blocks.STONE_SLAB, 1, 5))
-    );
-
     // --- Misc ---
 
     // board
@@ -176,36 +102,18 @@ public class StoneSawmillRecipesAdd {
     );
   }
 
-  private static void registerSawmillRecipeStone(IForgeRegistryModifiable<StoneSawmillRecipe> registry, String name, ItemStack output, Ingredient input) {
-
-    registry.register(new StoneSawmillRecipe(
-        output.copy(),
-        input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 4 * 20,
-        Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.IRON_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
-        0
-    ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_iron"));
-
-    registry.register(new StoneSawmillRecipe(
-        output.copy(),
-        input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 2 * 20,
-        Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.DIAMOND_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
-        0
-    ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_diamond"));
-  }
-
   private static void registerSawmillRecipeWood(IForgeRegistryModifiable<StoneSawmillRecipe> registry, String name, ItemStack output, Ingredient input) {
 
+    int woodChips = 8;
     output = output.copy();
     output.setCount(1);
 
     registry.register(new StoneSawmillRecipe(
         output,
         input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 4 * 20,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 8 * 20,
         Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.STONE_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
-        8
+        woodChips
     ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_tier_0"));
 
     output = output.copy();
@@ -214,12 +122,12 @@ public class StoneSawmillRecipesAdd {
     registry.register(new StoneSawmillRecipe(
         output,
         input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 3 * 20,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 6 * 20,
         Ingredient.fromStacks(
             new ItemStack(ModuleTechMachine.Items.FLINT_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE),
             new ItemStack(ModuleTechMachine.Items.BONE_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)
         ),
-        8
+        woodChips
     ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_tier_1"));
 
     output = output.copy();
@@ -228,9 +136,9 @@ public class StoneSawmillRecipesAdd {
     registry.register(new StoneSawmillRecipe(
         output,
         input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 2 * 20,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 4 * 20,
         Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.IRON_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
-        8
+        woodChips
     ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_tier_2"));
 
     output = output.copy();
@@ -239,9 +147,9 @@ public class StoneSawmillRecipesAdd {
     registry.register(new StoneSawmillRecipe(
         output,
         input,
-        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 20,
+        ModuleTechMachineConfig.STONE_SAWMILL.INPUT_SLOT_SIZE * 2 * 20,
         Ingredient.fromStacks(new ItemStack(ModuleTechMachine.Items.DIAMOND_MILL_BLADE, 1, OreDictionary.WILDCARD_VALUE)),
-        8
+        woodChips
     ).setRegistryName(ModuleTechMachine.MOD_ID, name + "_tier_3"));
   }
 }
