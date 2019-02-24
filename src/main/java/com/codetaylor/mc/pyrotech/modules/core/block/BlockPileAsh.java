7e83357bf421cefba451b3be7b8a659048a86e20
Jason Taylor <jason@codetaylor.com>
2019-02-24 15:30:33 -0800

    Restrict harvest of ash / wood chips pile to shovel

--- src/main/java/com/codetaylor/mc/pyrotech/modules/core/block/BlockPileAsh.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/core/block/BlockPileAsh.java
@@ -5,9 +5,14 @@ import com.codetaylor.mc.pyrotech.modules.core.item.ItemMaterial;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class BlockPileAsh
     extends BlockPileBase {
@@ -27,5 +32,11 @@ public class BlockPileAsh
   protected ItemStack getDrop(World world, BlockPos pos, IBlockState state) {
 
     return ItemMaterial.EnumType.PIT_ASH.asStack();
+  }
+
+  @Override
+  public boolean canHarvestBlock(IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EntityPlayer player) {
+
+    return (player.getHeldItemMainhand().getItem() instanceof ItemSpade);
   }
 }
--- src/main/java/com/codetaylor/mc/pyrotech/modules/core/block/BlockPileWoodChips.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/core/block/BlockPileWoodChips.java
@@ -5,9 +5,14 @@ import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemSpade;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
 
 public class BlockPileWoodChips
     extends BlockPileBase {
@@ -27,5 +32,11 @@ public class BlockPileWoodChips
   protected ItemStack getDrop(World world, BlockPos pos, IBlockState state) {
 
     return new ItemStack(ModuleCore.Blocks.ROCK, 1, BlockRock.EnumType.WOOD_CHIPS.getMeta());
+  }
+
+  @Override
+  public boolean canHarvestBlock(IBlockAccess world, @Nonnull BlockPos pos, @Nonnull EntityPlayer player) {
+
+    return (player.getHeldItemMainhand().getItem() instanceof ItemSpade);
   }
 }
