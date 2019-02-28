69c62ee46a4785e1c8a08421f966d46d16fe5019
Jason Taylor <jason@codetaylor.com>
2019-02-28 10:18:31 -0800

    Add rock bag contents names to waila

--- src/main/java/com/codetaylor/mc/pyrotech/modules/storage/plugin/waila/BagProvider.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/storage/plugin/waila/BagProvider.java
@@ -1,5 +1,6 @@
 package com.codetaylor.mc.pyrotech.modules.storage.plugin.waila;
 
+import com.codetaylor.mc.pyrotech.Reference;
 import com.codetaylor.mc.pyrotech.library.spi.plugin.waila.BodyProviderAdapter;
 import com.codetaylor.mc.pyrotech.library.util.Util;
 import com.codetaylor.mc.pyrotech.library.util.plugin.waila.WailaUtil;
@@ -9,6 +10,10 @@ import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.translation.I18n;
+import org.apache.commons.lang3.StringUtils;
+import org.lwjgl.input.Keyboard;
 
 import javax.annotation.Nonnull;
 import java.util.List;
@@ -31,6 +36,7 @@ public class BagProvider
       TileBagBase tile = (TileBagBase) tileEntity;
       TileBagBase.StackHandler stackHandler = tile.getStackHandler();
       StringBuilder sb = new StringBuilder();
+      int maxDigits = 0;
 
       if (tile.isOpen()) {
 
@@ -39,6 +45,12 @@ public class BagProvider
 
           if (!stackInSlot.isEmpty()) {
             sb.append(WailaUtil.getStackRenderString(stackInSlot));
+
+            int digits = String.valueOf(stackInSlot.getCount()).length();
+
+            if (digits > maxDigits) {
+              maxDigits = digits;
+            }
           }
         }
 
@@ -52,6 +64,33 @@ public class BagProvider
           tile.getItemCount(),
           tile.getItemCapacity()
       ));
+
+      if (tile.isOpen()) {
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+            || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)) {
+
+          for (int i = 0; i < stackHandler.getSlots(); i++) {
+            ItemStack stackInSlot = stackHandler.getStackInSlot(i);
+
+            if (!stackInSlot.isEmpty()) {
+              String count = String.valueOf(stackInSlot.getCount());
+
+              if (count.length() < maxDigits) {
+                count = TextFormatting.DARK_GRAY + StringUtils.repeat("0", maxDigits - count.length()).concat(TextFormatting.YELLOW + count);
+
+              } else {
+                count = TextFormatting.YELLOW + count;
+              }
+
+              tooltip.add(" " + count + " " + TextFormatting.GOLD + stackInSlot.getDisplayName());
+            }
+          }
+
+        } else {
+          tooltip.add(I18n.translateToLocalFormatted("gui.pyrotech.tooltip.extended.shift", Reference.Tooltip.COLOR_EXTENDED_INFO, TextFormatting.GRAY));
+        }
+      }
     }
 
     return tooltip;
