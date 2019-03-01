c3c5f8f8b297845710e32ff97a0417106c5a1266
Jason Taylor <jason@codetaylor.com>
2019-03-01 10:43:22 -0800

    Add durability to bow drill / flint an tinder tooltips

--- src/main/java/com/codetaylor/mc/pyrotech/modules/ignition/item/ItemBowDrill.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/ignition/item/ItemBowDrill.java
@@ -1,11 +1,16 @@
 package com.codetaylor.mc.pyrotech.modules.ignition.item;
 
 import com.codetaylor.mc.pyrotech.modules.ignition.ModuleIgnitionConfig;
+import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class ItemBowDrill
     extends ItemIgniterBase {
@@ -30,6 +35,14 @@ public class ItemBowDrill
     if (player instanceof EntityPlayer
         && !((EntityPlayer) player).isCreative()) {
       stack.damageItem(1, player);
+    }
+  }
+
+  @Override
+  public void addInformation(ItemStack stack, @Nullable World world, List<String> tooltip, ITooltipFlag flag) {
+
+    if (this.getDamage(stack) == 0) {
+      tooltip.add(I18n.translateToLocalFormatted("gui.pyrotech.tooltip.durability.full", this.getMaxDamage(stack)));
     }
   }
 }
