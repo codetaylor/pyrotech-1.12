3192c416435556effabc180ed53bae74b1a0ab18
Jason Taylor <jason@codetaylor.com>
2019-04-04 13:36:32 -0700

    Fix player doesn't receive book on start

--- src/main/java/com/codetaylor/mc/pyrotech/modules/plugin/patchouli/event/PlayerJoinEventHandler.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/plugin/patchouli/event/PlayerJoinEventHandler.java
@@ -5,12 +5,15 @@ import com.codetaylor.mc.pyrotech.modules.plugin.patchouli.ModulePluginPatchouli
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
 public class PlayerJoinEventHandler {
 
   private static final String KEY = "pyrotech.start";
 
+  @SubscribeEvent
   public void on(PlayerEvent.PlayerLoggedInEvent event) {
 
     if (ModulePatchouliConfig.GIVE_BOOK_ON_START) {
