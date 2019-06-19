2578a38d7a7f8b60f5ef965668570f2a7c0dee45
Jason Taylor <jason@codetaylor.com>
2019-06-19 14:14:23 -0700

    Fix wood compat ordering

--- src/main/java/com/codetaylor/mc/pyrotech/modules/core/init/WoodCompatInitializer.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/core/init/WoodCompatInitializer.java
@@ -44,75 +44,75 @@ public final class WoodCompatInitializer {
     data.mods.put("biomesoplenty", WoodCompatInitializer.expand(
 
         // planks
-        "planks_0;log_0:4",
-        "planks_0:1;log_0:5",
-        "planks_0:2;log_0:6",
-        "planks_0:3;log_0:7",
-        "planks_0:4;log_1:4",
-        "planks_0:5;log_1:5",
-        "planks_0:6;log_1:6",
-        "planks_0:7;log_1:7",
-        "planks_0:8;log_2:4",
-        "planks_0:9;log_2:5",
-        "planks_0:10;log_2:6",
-        "planks_0:11;log_2:7",
-        "planks_0:12;log_3:4",
-        "planks_0:13;log_3:5",
-        "planks_0:14;log_3:6",
-        "planks_0:15;log_3:7",
+        "log_0:4;planks_0",
+        "log_0:5;planks_0:1",
+        "log_0:6;planks_0:2",
+        "log_0:7;planks_0:3",
+        "log_1:4;planks_0:4",
+        "log_1:5;planks_0:5",
+        "log_1:6;planks_0:6",
+        "log_1:7;planks_0:7",
+        "log_2:4;planks_0:8",
+        "log_2:5;planks_0:9",
+        "log_2:6;planks_0:10",
+        "log_2:7;planks_0:11",
+        "log_3:4;planks_0:12",
+        "log_3:5;planks_0:13",
+        "log_3:6;planks_0:14",
+        "log_3:7;planks_0:15",
 
         // slabs
-        "wood_slab_0;planks_0:0",
-        "wood_slab_0:1;planks_0:1",
-        "wood_slab_0:2;planks_0:2",
-        "wood_slab_0:3;planks_0:3",
-        "wood_slab_0:4;planks_0:4",
-        "wood_slab_0:5;planks_0:5",
-        "wood_slab_0:6;planks_0:6",
-        "wood_slab_0:7;planks_0:7",
-        "wood_slab_1:0;planks_0:8",
-        "wood_slab_1:1;planks_0:9",
-        "wood_slab_1:2;planks_0:10",
-        "wood_slab_1:3;planks_0:11",
-        "wood_slab_1:4;planks_0:12",
-        "wood_slab_1:5;planks_0:13",
-        "wood_slab_1:6;planks_0:14",
-        "wood_slab_1:7;planks_0:15"
+        "planks_0:0;wood_slab_0",
+        "planks_0:1;wood_slab_0:1",
+        "planks_0:2;wood_slab_0:2",
+        "planks_0:3;wood_slab_0:3",
+        "planks_0:4;wood_slab_0:4",
+        "planks_0:5;wood_slab_0:5",
+        "planks_0:6;wood_slab_0:6",
+        "planks_0:7;wood_slab_0:7",
+        "planks_0:8;wood_slab_1:0",
+        "planks_0:9;wood_slab_1:1",
+        "planks_0:10;wood_slab_1:2",
+        "planks_0:11;wood_slab_1:3",
+        "planks_0:12;wood_slab_1:4",
+        "planks_0:13;wood_slab_1:5",
+        "planks_0:14;wood_slab_1:6",
+        "planks_0:15;wood_slab_1:7"
     ));
 
     data.mods.put("natura", WoodCompatInitializer.expand(
 
         // planks
-        "overworld_planks;overworld_logs",
-        "overworld_planks:1;overworld_logs:1",
-        "overworld_planks:2;overworld_logs:2",
-        "overworld_planks:3;overworld_logs:3",
-        "overworld_planks:4;overworld_logs2:0",
-        "overworld_planks:5;overworld_logs2:1",
-        "overworld_planks:6;overworld_logs2:2",
-        "overworld_planks:7;overworld_logs2:3",
-        "overworld_planks:8;redwood_logs:*",
+        "overworld_logs;overworld_planks",
+        "overworld_logs:1;overworld_planks:1",
+        "overworld_logs:2;overworld_planks:2",
+        "overworld_logs:3;overworld_planks:3",
+        "overworld_logs2:0;overworld_planks:4",
+        "overworld_logs2:1;overworld_planks:5",
+        "overworld_logs2:2;overworld_planks:6",
+        "overworld_logs2:3;overworld_planks:7",
+        "redwood_logs:*;overworld_planks:8",
 
-        "nether_planks:0;nether_logs:0",
-        "nether_planks:2;nether_logs:2",
-        "nether_planks:3;nether_logs:3",
-        "nether_planks:1;nether_logs2:*",
+        "nether_logs:0;nether_planks:0",
+        "nether_logs:2;nether_planks:2",
+        "nether_logs:3;nether_planks:3",
+        "nether_logs2:*;nether_planks:1",
 
         // slabs
-        "overworld_slab;overworld_planks",
-        "overworld_slab:1;overworld_planks:1",
-        "overworld_slab:2;overworld_planks:2",
-        "overworld_slab:3;overworld_planks:3",
-        "overworld_slab:4;overworld_planks:4",
-        "overworld_slab2:0;overworld_planks:5",
-        "overworld_slab2:1;overworld_planks:6",
-        "overworld_slab2:2;overworld_planks:7",
-        "overworld_slab2:3;overworld_planks:8",
+        "overworld_planks;overworld_slab",
+        "overworld_planks:1;overworld_slab:1",
+        "overworld_planks:2;overworld_slab:2",
+        "overworld_planks:3;overworld_slab:3",
+        "overworld_planks:4;overworld_slab:4",
+        "overworld_planks:5;overworld_slab2:0",
+        "overworld_planks:6;overworld_slab2:1",
+        "overworld_planks:7;overworld_slab2:2",
+        "overworld_planks:8;overworld_slab2:3",
 
-        "nether_slab:0;nether_planks:0",
-        "nether_slab:1;nether_planks:1",
-        "nether_slab:2;nether_planks:2",
-        "nether_slab:3;nether_planks:3"
+        "nether_planks:0;nether_slab:0",
+        "nether_planks:1;nether_slab:1",
+        "nether_planks:2;nether_slab:2",
+        "nether_planks:3;nether_slab:3"
     ));
 
     // TODO: modded planks and slabs
@@ -129,7 +129,7 @@ public final class WoodCompatInitializer {
         "This file defines input and output pairs for auto-generating recipes",
         "for the Chopping Block.",
         "",
-        "Entries are in the format (output);(input)",
+        "Entries are in the format (input);(output)",
         "Entry item strings are in the format: (path):(meta)"
     };
 
@@ -146,27 +146,6 @@ public final class WoodCompatInitializer {
     }
 
     return result;
-  }
-
-  public static class WoodCompatModData {
-
-    public Map<String, String> planks = new TreeMap<>();
-    public Map<String, String> slabs = new TreeMap<>();
-
-    public WoodCompatModData planks(String... strings) {
-
-      return this;
-    }
-
-    public WoodCompatModData slabs(String... strings) {
-
-      for (String slab : strings) {
-        String[] split = slab.split(";");
-        this.planks.put(split[0], split[1]);
-      }
-
-      return this;
-    }
   }
 
   private WoodCompatInitializer() {
