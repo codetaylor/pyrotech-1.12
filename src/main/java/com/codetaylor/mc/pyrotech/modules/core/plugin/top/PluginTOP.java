3ef75ad60e5c086d11b06393c4ac49b41cf6129a
Jason Taylor <jason@codetaylor.com>
2019-07-10 13:45:56 -0700

    Implement localized tank element for TOP

--- src/main/java/com/codetaylor/mc/pyrotech/modules/core/plugin/top/PluginTOP.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/core/plugin/top/PluginTOP.java
@@ -1,16 +1,21 @@
 package com.codetaylor.mc.pyrotech.modules.core.plugin.top;
 
 import com.codetaylor.mc.pyrotech.library.util.Util;
+import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
+import com.google.common.base.Preconditions;
 import io.netty.buffer.ByteBuf;
 import mcjty.theoneprobe.api.IElement;
 import mcjty.theoneprobe.api.ITheOneProbe;
 import mcjty.theoneprobe.apiimpl.client.ElementTextRender;
 import mcjty.theoneprobe.network.NetworkTools;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.fluids.FluidStack;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
@@ -18,6 +23,7 @@ public class PluginTOP {
 
   private static int ELEMENT_TEXT_LOCALIZED;
   private static int ELEMENT_ITEM_LABEL;
+  private static int ELEMENT_TANK_LABEL;
 
   public static class Callback
       implements Function<ITheOneProbe, Void> {
@@ -27,6 +33,7 @@ public class PluginTOP {
 
       PluginTOP.ELEMENT_TEXT_LOCALIZED = top.registerElementFactory(ElementTextLocalized::new);
       PluginTOP.ELEMENT_ITEM_LABEL = top.registerElementFactory(ElementItemLabel::new);
+      PluginTOP.ELEMENT_TANK_LABEL = top.registerElementFactory(ElementTankLabel::new);
       return null;
     }
   }
@@ -295,6 +302,119 @@ public class PluginTOP {
     public int getID() {
 
       return PluginTOP.ELEMENT_TEXT_LOCALIZED;
+    }
+  }
+
+  public static class ElementTankLabel
+      implements IElement {
+
+    private final String textFormatting;
+    private final FluidStack fluidStack;
+    private final int capacity;
+
+    private String renderText;
+    public static final String LANG_KEY = "gui." + ModuleCore.MOD_ID + ".waila.tank.fluid";
+
+    public ElementTankLabel(@Nullable TextFormatting textFormatting, FluidStack fluidStack, int capacity) {
+
+      if (textFormatting == null) {
+        this.textFormatting = null;
+
+      } else {
+        this.textFormatting = textFormatting.toString();
+      }
+
+      this.fluidStack = Preconditions.checkNotNull(fluidStack);
+      this.capacity = capacity;
+    }
+
+    public ElementTankLabel(ByteBuf buf) {
+
+      int length = buf.readInt();
+
+      if (length > 0) {
+        this.textFormatting = new PacketBuffer(buf).readString(length);
+
+      } else {
+        this.textFormatting = null;
+      }
+
+      NBTTagCompound compound;
+
+      try {
+        compound = new PacketBuffer(buf).readCompoundTag();
+
+      } catch (IOException e) {
+        this.fluidStack = null;
+        throw new RuntimeException("", e);
+      }
+
+      this.capacity = buf.readInt();
+
+      if (compound != null) {
+        this.fluidStack = FluidStack.loadFluidStackFromNBT(compound);
+
+      } else {
+        this.fluidStack = null;
+      }
+
+      if (this.fluidStack != null) {
+        StringBuilder builder = new StringBuilder();
+
+        if (this.textFormatting != null) {
+          String localizedName = this.fluidStack.getLocalizedName();
+          this.renderText = this.textFormatting
+              + Util.translateFormatted(LANG_KEY, localizedName, this.fluidStack.amount, this.capacity);
+        }
+      }
+    }
+
+    @Override
+    public void render(int x, int y) {
+
+      if (this.renderText != null) {
+        ElementTextRender.render(this.renderText, x, y);
+      }
+    }
+
+    @Override
+    public int getWidth() {
+
+      if (this.renderText != null) {
+        return ElementTextRender.getWidth(this.renderText);
+
+      } else {
+        return 10;
+      }
+    }
+
+    @Override
+    public int getHeight() {
+
+      return 10;
+    }
+
+    @Override
+    public void toBytes(ByteBuf buf) {
+
+      if (this.textFormatting == null) {
+        buf.writeInt(0);
+
+      } else {
+        buf.writeInt(this.textFormatting.length());
+        new PacketBuffer(buf).writeString(this.textFormatting);
+      }
+
+      NBTTagCompound compound = this.fluidStack.writeToNBT(new NBTTagCompound());
+      new PacketBuffer(buf).writeCompoundTag(compound);
+
+      buf.writeInt(this.capacity);
+    }
+
+    @Override
+    public int getID() {
+
+      return PluginTOP.ELEMENT_TANK_LABEL;
     }
   }
 
