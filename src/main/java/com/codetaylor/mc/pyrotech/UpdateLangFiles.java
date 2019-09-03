efd378390b1871838c2c038cf38c18ee957a1e37
Jason Taylor <jason@codetaylor.com>
2019-09-02 18:39:31 -0700

    Cleanup lang file updater

--- src/main/java/com/codetaylor/mc/pyrotech/UpdateLangFiles.java
+++ src/main/java/com/codetaylor/mc/pyrotech/UpdateLangFiles.java
@@ -10,14 +10,16 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * Updates all lang files from one master lang file.
- *
+ * <p>
  * Overwrites each lang file with a master lang file while translating any values
  * that already exist in the overwritten lang file.
  */
@@ -27,99 +29,167 @@ public class UpdateLangFiles {
 
   public static void main(String... args) {
 
-    String modId = "pyrotech";
+    new LangFileUpdater(
+        "pyrotech",
+        "en_us.lang",
+        ".",
+        new FileLocator(),
+        new Converter(),
+        new Writer()
+    ).update();
+  }
 
-    Path langPath = Paths.get("src/main/resources/assets/" + modId + "/lang/");
-    Path masterFilePath = langPath.resolve("en_us.lang");
-    Path outputPath = Paths.get(".");
-    List<Path> langFileList;
+  public static class LangFileUpdater {
 
-    try (Stream<Path> paths = Files.walk(langPath)) {
-      langFileList = paths
-          .filter(Files::isRegularFile)
-          .filter(path -> path.toString().endsWith(".lang")
-              && !path.toString().endsWith(masterFilePath.toString()))
-          .sorted()
-          .collect(Collectors.toList());
+    private final Path langPath;
+    private final Path masterFilePath;
+    private final Path outputPath;
+    private FileLocator fileLocator;
+    private Converter converter;
+    private Writer writer;
 
-    } catch (IOException e) {
-      LOGGER.error("", e);
-      return;
+    public LangFileUpdater(String modId, String masterLangFile, String outputPath, FileLocator fileLocator, Converter converter, Writer writer) {
+
+      this.langPath = Paths.get("src/main/resources/assets/" + modId + "/lang/");
+      this.masterFilePath = this.langPath.resolve(masterLangFile);
+      this.outputPath = Paths.get(outputPath);
+      this.fileLocator = fileLocator;
+      this.converter = converter;
+      this.writer = writer;
     }
 
-    // Read master file
-    List<String> masterList;
+    public void update() {
 
-    try {
-      masterList = new Reader().read(masterFilePath, new ArrayList<>());
+      // Read master file
+      List<String> masterList;
 
-    } catch (IOException e) {
-      LOGGER.error("Unable to read master file: " + masterFilePath, e);
-      return;
+      try {
+        masterList = new Reader().read(this.masterFilePath, new ArrayList<>());
+
+      } catch (IOException e) {
+        LOGGER.error("Unable to read master file: " + this.masterFilePath, e);
+        return;
+      }
+
+      List<Path> langFileList = this.fileLocator.locate(
+          this.langPath,
+          new PathFilter(this.masterFilePath.toString())
+      );
+
+      for (Path path : langFileList) {
+        this.converter.convert(path, this.outputPath, masterList, this.writer);
+      }
     }
 
-    for (Path path : langFileList) {
-      process(path, outputPath, masterList);
+  }
+
+  public static class Converter {
+
+    public void convert(Path subFilePath, Path outputPath, List<String> masterList, Writer writer) {
+
+      Properties subProperties;
+
+      try {
+        subProperties = new PropertiesReader().read(subFilePath);
+
+      } catch (IOException e) {
+        LOGGER.error("Unable to load lang file: " + subFilePath, e);
+        return;
+      }
+
+      // Output
+      List<String> outputList = new ArrayList<>(masterList.size());
+      List<String> newKeys = new ArrayList<>();
+
+      for (String masterLine : masterList) {
+        String masterLineTrimmed = masterLine.trim();
+
+        // master line is empty
+        if (masterLineTrimmed.length() == 0) {
+          outputList.add("");
+          continue;
+        }
+
+        // master line is comment
+        if (masterLineTrimmed.startsWith("#")) {
+          outputList.add(masterLineTrimmed);
+          continue;
+        }
+
+        // master line is not a comment
+        // look up the translated value
+        String[] split = masterLineTrimmed.split("=");
+        String property = subProperties.getProperty(split[0]);
+
+        if (property == null) {
+          // has no translated value
+          // add the master line
+          outputList.add(masterLineTrimmed);
+          newKeys.add(split[0]);
+          continue;
+        }
+
+        // has translated value
+        // concatenate and add the translated line
+        outputList.add(split[0] + "=" + property);
+      }
+
+      try {
+        outputPath = outputPath.resolve(subFilePath);
+        writer.write(outputPath, outputList);
+        LOGGER.info("Wrote " + newKeys.size() + " new lines to " + outputPath + "\n" + String.join("\n", newKeys));
+
+      } catch (IOException e) {
+        LOGGER.error("Failed to write file: " + outputPath, e);
+      }
     }
   }
 
-  protected static void process(Path subFilePath, Path outputPath, List<String> masterList) {
+  public static class PathFilter
+      implements Predicate<Path> {
 
-    // Read sub file
-    Properties subProperties = new Properties();
+    private final String masterFilePathString;
 
-    try {
-      subProperties.load(Files.newBufferedReader(subFilePath));
+    public PathFilter(String masterFilePathString) {
 
-    } catch (IOException e) {
-      LOGGER.error("Unable to read sub file as property file: " + subFilePath, e);
-      return;
+      this.masterFilePathString = masterFilePathString;
     }
 
-    // Output
-    List<String> outputList = new ArrayList<>(masterList.size());
-    List<String> newKeys = new ArrayList<>();
+    @Override
+    public boolean test(Path path) {
 
-    for (String masterLine : masterList) {
-      String masterLineTrimmed = masterLine.trim();
-
-      // master line is empty
-      if (masterLineTrimmed.length() == 0) {
-        outputList.add("");
-        continue;
-      }
-
-      // master line is comment
-      if (masterLineTrimmed.startsWith("#")) {
-        outputList.add(masterLineTrimmed);
-        continue;
-      }
-
-      // master line is not a comment
-      // look up the translated value
-      String[] split = masterLineTrimmed.split("=");
-      String property = subProperties.getProperty(split[0]);
-
-      if (property == null) {
-        // has no translated value
-        // add the master line
-        outputList.add(masterLineTrimmed);
-        newKeys.add(split[0]);
-        continue;
-      }
-
-      // has translated value
-      // concatenate and add the translated line
-      outputList.add(split[0] + "=" + property);
+      String pathString = path.toString();
+      return pathString.endsWith(".lang")
+          && !pathString.endsWith(this.masterFilePathString);
     }
+  }
 
-    try {
-      outputPath = outputPath.resolve(subFilePath);
-      new Writer().write(outputPath, outputList);
-      LOGGER.info("Wrote " + newKeys.size() + " new lines to " + outputPath + "\n" + String.join("\n", newKeys));
+  public static class FileLocator {
 
-    } catch (IOException e) {
-      LOGGER.error("Failed to write file: " + outputPath, e);
+    public List<Path> locate(Path langPath, Predicate<? super Path> filter) {
+
+      try (Stream<Path> paths = Files.walk(langPath)) {
+        return paths
+            .filter(Files::isRegularFile)
+            .filter(filter)
+            .sorted()
+            .collect(Collectors.toList());
+
+      } catch (IOException e) {
+        LOGGER.error("", e);
+        return Collections.emptyList();
+      }
+    }
+  }
+
+  public static class PropertiesReader {
+
+    public Properties read(Path path) throws IOException {
+
+      // Read sub file
+      Properties subProperties = new Properties();
+      subProperties.load(Files.newBufferedReader(path));
+      return subProperties;
     }
   }
 
