8a5cd24458c663b65cafb82df2b9e142a0d4fc50
Jason Taylor <jason@codetaylor.com>
2020-04-20 17:32:35 -0700

    Factor out campfire effect base class

---
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/potion/PotionCampfireBase.java
@@ -0,0 +1,65 @@
+package com.codetaylor.mc.pyrotech.modules.tech.basic.potion;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class PotionCampfireBase
+    extends Potion {
+
+  public PotionCampfireBase(boolean isBadEffectIn, int liquidColorIn) {
+
+    super(isBadEffectIn, liquidColorIn);
+  }
+
+  protected abstract ResourceLocation getTexture();
+
+  @SideOnly(Side.CLIENT)
+  @Override
+  public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+
+    if (mc.currentScreen != null) {
+      mc.getTextureManager().bindTexture(this.getTexture());
+      Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+    }
+  }
+
+  @SideOnly(Side.CLIENT)
+  @Override
+  public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+
+    mc.getTextureManager().bindTexture(this.getTexture());
+    Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+  }
+
+  @Override
+  public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, @Nonnull EntityLivingBase entity, int amplifier, double health) {
+
+    //
+  }
+
+  @Override
+  public void performEffect(@Nonnull EntityLivingBase entity, int amplifier) {
+
+    //
+  }
+
+  @Nonnull
+  @Override
+  public List<ItemStack> getCurativeItems() {
+
+    return Collections.emptyList();
+  }
+}
--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/potion/PotionComfort.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/potion/PotionComfort.java
@@ -2,21 +2,14 @@ package com.codetaylor.mc.pyrotech.modules.tech.basic.potion;
 
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasic;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasicConfig;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Gui;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.potion.Potion;
-import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
+import javax.annotation.Nonnull;
 import java.awt.*;
 
 public class PotionComfort
-    extends Potion {
+    extends PotionCampfireBase {
 
   public static final String NAME = "comfort";
 
@@ -29,26 +22,14 @@ public class PotionComfort
     super(false, Color.BLACK.getRGB());
   }
 
-  @SideOnly(Side.CLIENT)
   @Override
-  public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+  protected ResourceLocation getTexture() {
 
-    if (mc.currentScreen != null) {
-      mc.getTextureManager().bindTexture(TEXTURE);
-      Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
-    }
-  }
-
-  @SideOnly(Side.CLIENT)
-  @Override
-  public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
-
-    mc.getTextureManager().bindTexture(TEXTURE);
-    Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
+    return TEXTURE;
   }
 
   @Override
-  public void performEffect(EntityLivingBase entity, int amplifier) {
+  public void performEffect(@Nonnull EntityLivingBase entity, int amplifier) {
 
     if (entity.getHealth() < entity.getMaxHealth()) {
       int amount = Math.max(0, ModuleTechBasicConfig.CAMPFIRE_EFFECTS.COMFORT_REGEN_HALF_HEARTS);
@@ -57,12 +38,6 @@ public class PotionComfort
         entity.heal(amount);
       }
     }
-  }
-
-  @Override
-  public void affectEntity(@Nullable Entity source, @Nullable Entity indirectSource, EntityLivingBase entityLivingBaseIn, int amplifier, double health) {
-
-    //
   }
 
   @Override
