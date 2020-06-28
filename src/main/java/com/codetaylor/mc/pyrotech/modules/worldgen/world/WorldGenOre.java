baee06f090a66df91fca8a229a127c790eb3cfef
codetaylor <jason@codetaylor.com>
2020-06-27 22:50:17 -0700

    Cleanup code

--- src/main/java/com/codetaylor/mc/pyrotech/modules/worldgen/world/WorldGenOre.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/worldgen/world/WorldGenOre.java
@@ -38,12 +38,12 @@ public class WorldGenOre
     int numberOfBlocks = this.countSupplier.get(rand);
 
     float f = rand.nextFloat() * (float) Math.PI;
-    double d0 = (double) ((float) (position.getX() + 8) + MathHelper.sin(f) * (float) numberOfBlocks / 8.0F);
-    double d1 = (double) ((float) (position.getX() + 8) - MathHelper.sin(f) * (float) numberOfBlocks / 8.0F);
-    double d2 = (double) ((float) (position.getZ() + 8) + MathHelper.cos(f) * (float) numberOfBlocks / 8.0F);
-    double d3 = (double) ((float) (position.getZ() + 8) - MathHelper.cos(f) * (float) numberOfBlocks / 8.0F);
-    double d4 = (double) (position.getY() + rand.nextInt(3) - 2);
-    double d5 = (double) (position.getY() + rand.nextInt(3) - 2);
+    double d0 = (float) (position.getX() + 8) + MathHelper.sin(f) * (float) numberOfBlocks / 8.0F;
+    double d1 = (float) (position.getX() + 8) - MathHelper.sin(f) * (float) numberOfBlocks / 8.0F;
+    double d2 = (float) (position.getZ() + 8) + MathHelper.cos(f) * (float) numberOfBlocks / 8.0F;
+    double d3 = (float) (position.getZ() + 8) - MathHelper.cos(f) * (float) numberOfBlocks / 8.0F;
+    double d4 = position.getY() + rand.nextInt(3) - 2;
+    double d5 = position.getY() + rand.nextInt(3) - 2;
 
     for (int i = 0; i < numberOfBlocks; ++i) {
       float f1 = (float) i / (float) numberOfBlocks;
@@ -111,7 +111,7 @@ public class WorldGenOre
   public static class BlockPredicate
       implements Predicate<IBlockState> {
 
-    private Block toReplace;
+    private final Block toReplace;
 
     public BlockPredicate(Block toReplace) {
 
