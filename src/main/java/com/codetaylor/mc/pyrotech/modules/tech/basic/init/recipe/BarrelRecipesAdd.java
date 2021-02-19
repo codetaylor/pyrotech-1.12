378ab8475ccf6a4374ba4d5d72e268a2b46e7b7b
codetaylor <jason@codetaylor.com>
2021-02-18 17:28:27 -0800

    Add barrel recipe for tannin

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/BarrelRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/BarrelRecipesAdd.java
@@ -1,29 +1,32 @@
 package com.codetaylor.mc.pyrotech.modules.tech.basic.init.recipe;
 
-import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
+import com.codetaylor.mc.pyrotech.ModPyrotech;
+import com.codetaylor.mc.pyrotech.modules.hunting.ModuleHunting;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasic;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.recipe.BarrelRecipe;
-import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.oredict.OreIngredient;
 import net.minecraftforge.registries.IForgeRegistry;
 
 public class BarrelRecipesAdd {
 
   public static void apply(IForgeRegistry<BarrelRecipe> registry) {
 
-    registry.register(new BarrelRecipe(
-        new FluidStack(ModuleCore.Fluids.CLAY, 1000),
-        new Ingredient[]{
-            Ingredient.fromStacks(new ItemStack(Blocks.DIRT)),
-            Ingredient.fromStacks(new ItemStack(Blocks.DIRT)),
-            Ingredient.fromStacks(new ItemStack(Blocks.DIRT))
-        },
-        new FluidStack(FluidRegistry.WATER, 1000),
-        20 * 10
-    ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "clay")));
+    if (ModPyrotech.INSTANCE.isModuleEnabled(ModuleHunting.class)) {
+      registry.register(new BarrelRecipe(
+          new FluidStack(ModuleHunting.Fluids.TANNIN, 1000),
+          new Ingredient[]{
+              new OreIngredient("treeLeaves"),
+              new OreIngredient("treeLeaves"),
+              new OreIngredient("treeLeaves"),
+              new OreIngredient("treeLeaves")
+          },
+          new FluidStack(FluidRegistry.WATER, 1000),
+          20 * 60 * 10
+      ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "tannin")));
+    }
   }
 }
