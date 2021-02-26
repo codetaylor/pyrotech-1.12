9384f79e01aeeb5445a7d62065310e21b1846de8
codetaylor <jason@codetaylor.com>
2021-02-25 17:25:16 -0800

    Add leather recipes

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/TanningRackRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/TanningRackRecipesAdd.java
@@ -1,6 +1,7 @@
 package com.codetaylor.mc.pyrotech.modules.tech.basic.init.recipe;
 
 import com.codetaylor.mc.pyrotech.ModPyrotech;
+import com.codetaylor.mc.pyrotech.modules.core.item.ItemMaterial;
 import com.codetaylor.mc.pyrotech.modules.hunting.ModuleHunting;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasic;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.recipe.TanningRackRecipe;
@@ -17,10 +18,17 @@ public class TanningRackRecipesAdd {
     if (ModPyrotech.INSTANCE.isModuleEnabled(ModuleHunting.class)) {
       registry.register(new TanningRackRecipe(
           new ItemStack(Items.LEATHER),
-          Ingredient.fromStacks(new ItemStack(ModuleHunting.Items.HIDE_WASHED)),
-          ItemStack.EMPTY,
-          20 * 10
-      ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "test_recipe")));
+          Ingredient.fromStacks(new ItemStack(ModuleHunting.Items.HIDE_TANNED)),
+          new ItemStack(ModuleHunting.Items.HIDE_WASHED),
+          10 * 60 * 20
+      ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "leather")));
+
+      registry.register(new TanningRackRecipe(
+          ItemMaterial.EnumType.LEATHER_SMALL.asStack(),
+          Ingredient.fromStacks(new ItemStack(ModuleHunting.Items.HIDE_SMALL_TANNED)),
+          new ItemStack(ModuleHunting.Items.HIDE_SMALL_WASHED),
+          10 * 60 * 20
+      ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "leather_small")));
     }
   }
 }
