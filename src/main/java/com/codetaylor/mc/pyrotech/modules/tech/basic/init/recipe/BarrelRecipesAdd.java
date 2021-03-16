704c1459aa2abf770053b544c4a7ec6a15f705bc
codetaylor <jason@codetaylor.com>
2021-03-16 09:57:39 -0700

    Add barrel recipe for pyroberry wine

--- src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/BarrelRecipesAdd.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/tech/basic/init/recipe/BarrelRecipesAdd.java
@@ -1,9 +1,11 @@
 package com.codetaylor.mc.pyrotech.modules.tech.basic.init.recipe;
 
 import com.codetaylor.mc.pyrotech.ModPyrotech;
+import com.codetaylor.mc.pyrotech.modules.core.ModuleCore;
 import com.codetaylor.mc.pyrotech.modules.hunting.ModuleHunting;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.ModuleTechBasic;
 import com.codetaylor.mc.pyrotech.modules.tech.basic.recipe.BarrelRecipe;
+import net.minecraft.init.Items;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.FluidRegistry;
@@ -28,5 +30,17 @@ public class BarrelRecipesAdd {
           20 * 60 * 10
       ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "tannin")));
     }
+
+    registry.register(new BarrelRecipe(
+        new FluidStack(ModuleCore.Fluids.PYROBERRY_WINE, 1000),
+        new Ingredient[]{
+            Ingredient.fromItems(ModuleCore.Items.PYROBERRIES),
+            Ingredient.fromItems(ModuleCore.Items.PYROBERRIES),
+            Ingredient.fromItems(ModuleCore.Items.PYROBERRIES),
+            Ingredient.fromItems(Items.SUGAR)
+        },
+        new FluidStack(FluidRegistry.WATER, 1000),
+        20 * 60 * 10
+    ).setRegistryName(new ResourceLocation(ModuleTechBasic.MOD_ID, "pyroberry_wine")));
   }
 }
