70e9aeed129fda89ba97e6c66a3fb4a4a899f238
codetaylor <jason@codetaylor.com>
2021-03-18 12:24:12 -0700

    Fix gloamberries item

--- src/main/java/com/codetaylor/mc/pyrotech/modules/core/item/ItemGloamberries.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/core/item/ItemGloamberries.java
@@ -1,31 +1,16 @@
 package com.codetaylor.mc.pyrotech.modules.core.item;
 
 import com.codetaylor.mc.pyrotech.modules.core.ModuleCoreConfig;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemFood;
-import net.minecraft.item.ItemStack;
-import net.minecraft.world.World;
 
-import javax.annotation.Nonnull;
-import javax.annotation.ParametersAreNonnullByDefault;
-
-public class ItemPyroberries
+public class ItemGloamberries
     extends ItemFood {
 
-  public static final String NAME = "pyroberries";
+  public static final String NAME = "gloamberries";
 
-  public ItemPyroberries() {
+  public ItemGloamberries() {
 
-    super(ModuleCoreConfig.FOOD.PYROBERRIES_HUNGER, (float) ModuleCoreConfig.FOOD.PYROBERRIES_SATURATION, false);
+    super(ModuleCoreConfig.FOOD.GLOAMBERRIES_HUNGER, (float) ModuleCoreConfig.FOOD.GLOAMBERRIES_SATURATION, false);
     this.setAlwaysEdible();
-  }
-
-  @ParametersAreNonnullByDefault
-  @Nonnull
-  @Override
-  public ItemStack onItemUseFinish(ItemStack stack, World world, EntityLivingBase entityLiving) {
-
-    entityLiving.setFire(5);
-    return super.onItemUseFinish(stack, world, entityLiving);
   }
 }
