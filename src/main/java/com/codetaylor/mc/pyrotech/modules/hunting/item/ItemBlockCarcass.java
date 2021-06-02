1d262b701ce32197ba04c38d2cd54a5b68c0452a
codetaylor <jason@codetaylor.com>
2021-06-01 21:29:51 -0700

    Add carcass item utility methods

--- src/main/java/com/codetaylor/mc/pyrotech/modules/hunting/item/ItemBlockCarcass.java
+++ src/main/java/com/codetaylor/mc/pyrotech/modules/hunting/item/ItemBlockCarcass.java
@@ -1,7 +1,11 @@
 package com.codetaylor.mc.pyrotech.modules.hunting.item;
 
+import com.codetaylor.mc.athenaeum.util.StackHelper;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.items.ItemStackHandler;
 
 public class ItemBlockCarcass
     extends ItemBlock {
@@ -10,5 +14,37 @@ public class ItemBlockCarcass
 
     super(block);
     this.setMaxStackSize(1);
+  }
+
+  public static ItemStackHandler getItemStackHandler(ItemStack itemStack) {
+
+    NBTTagCompound tag = itemStack.getTagCompound();
+
+    if (tag == null || !tag.hasKey(StackHelper.BLOCK_ENTITY_TAG)) {
+      return null;
+    }
+
+    NBTTagCompound tileTag = tag.getCompoundTag(StackHelper.BLOCK_ENTITY_TAG);
+
+    if (!tileTag.hasKey("stackHandler")) {
+      return null;
+    }
+
+    ItemStackHandler stackHandler = new ItemStackHandler();
+    stackHandler.deserializeNBT(tileTag.getCompoundTag("stackHandler"));
+    return stackHandler;
+  }
+
+  public static void updateItemStackHandler(ItemStack itemStack, ItemStackHandler stackHandler) {
+
+    NBTTagCompound tag = itemStack.getTagCompound();
+
+    if (tag == null || !tag.hasKey(StackHelper.BLOCK_ENTITY_TAG)) {
+      return;
+    }
+
+    NBTTagCompound tileTag = tag.getCompoundTag(StackHelper.BLOCK_ENTITY_TAG);
+    tileTag.setTag("stackHandler", stackHandler.serializeNBT());
+    itemStack.setTagCompound(tag);
   }
 }
